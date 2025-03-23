4446b24e73b5a3178a848ac452b7af9b90ed48d9
Daniel learn <danielzeta187@gmail.com>
2025-03-23 12:52:09 -0500

    borrar cosas de func.py

--- func.py
+++ func.py
@@ -6,13 +6,4 @@ print(suma(2, 2, 2, 2, 3, 4, 5, 6, 4))
 
 
 #funcion que si suma la cantidad de numeros que se quiera
-
-# hola mundo bello de este mundo
-# hola mundo
-hola = 3
-
-buenos_dias = 4
-
-la_vida_es_bella = buenos_dias - hola
-
-print(la_vida_es_bella)
+# se borra lo que habia
