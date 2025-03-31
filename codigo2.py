808a4c85ea7297c1b2fb0438f59291221c54a2bc
Daniel <dzambranom@udenar.edu.co>
2025-03-31 17:25:31 -0500

    definir funcion saludar_bien

--- codigo2.py
+++ codigo2.py
@@ -3,3 +3,8 @@ apellido =  "zambrano mora" #apellido completo
 
 def saludo (nombre, apellido):
     print("hola " + nombre +  " " + apellido)
+    
+def saludar_bien (nombre_completo):
+    print(f'hola soy ${nombre_completo}, ¿como estas?')
+    
+saludar_bien("daniel zambrano")
