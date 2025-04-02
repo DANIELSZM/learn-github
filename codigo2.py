3392982c52c34bb5d89ced7765924aa19bca719d
Daniel learn <danielzeta187@gmail.com>
2025-04-02 16:45:04 -0500

    ver funcion saludar_bien y saludo en el terminal

--- codigo2.py
+++ codigo2.py
@@ -2,9 +2,13 @@ nombre = "daniel stiven"
 apellido =  "zambrano mora" #apellido completo
 
 def saludo (nombre, apellido):
-    print("hola " + nombre +  " " + apellido)
+    print("hola soy " + nombre +  " " + apellido)
     
 def saludar_bien (nombre_completo):
-    print(f'hola soy ${nombre_completo}, ¿como estas?')
+    print(f'hola soy {nombre_completo}, ¿como estas?')
     
-saludar_bien("daniel zambrano")
+print("funcion saludar bien:  ")
+saludar_bien("daniel zambrano") 
+
+print("\nfuncion saludo:  ")
+saludo("daniel", "zambrano") # llamar funcion saludo y saludar bien para ver la diferencia
