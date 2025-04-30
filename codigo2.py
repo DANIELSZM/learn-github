0f9a9f7b59ec151c4e005028e50858541aad4da4
Daniel learn <danielzeta187@gmail.com>
2025-04-30 15:18:54 -0500

    update codigo2.py

--- codigo2.py
+++ codigo2.py
@@ -1,8 +1,6 @@
-nombre = "daniel stiven"
-apellido =  "zambrano mora" #apellido completo
-    
-def saludar_bien (nombre_completo):
+#funcion saludar 
+def saludar (nombre_completo):
     print(f'hola soy {nombre_completo}, ¿como estas?')
     
-print("funcion saludar bien:  ")
-saludar_bien("daniel zambrano") 
+print("funcion saludar:  ")
+saludar("daniel zambrano") 
